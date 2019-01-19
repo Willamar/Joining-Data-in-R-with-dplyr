e4fe489440e7cb054824778c23b47bf3acb0f8c4
Willamar <wfs20@hotmail.com>
2019-01-18 22:03:16 -0200

    Adicionado novos comandos de join de datasets

---
+++ Assembling data.R
@@ -0,0 +1,56 @@
+# Examine side_one and side_two
+side_one
+side_two
+
+# Bind side_one and side_two into a single dataset
+side_one %>% 
+  bind_rows(side_two)
+
+
+# Examine discography and jimi
+discography
+jimi
+
+jimi %>% 
+  # Bind jimi by rows, with ID column "album"
+  bind_rows(.id = "album") %>% 
+  # Make a complete data frame
+  left_join(discography, by = "album")
+
+# Examine hank_years and hank_charts
+hank_years
+hank_charts
+
+hank_years %>% 
+  # Reorder hank_years alphabetically by song title
+  arrange(song) %>% 
+  # Select just the year column
+  select(year) %>% 
+  # Bind the year column
+  bind_cols(hank_charts) %>% 
+  # Arrange the finished dataset
+  arrange(year, song)
+
+
+# Make combined data frame using data_frame()
+data_frame(year = hank_year, song = hank_song, peak = hank_peak) %>% 
+  # Extract songs where peak equals 1
+  filter(peak == 1)
+
+# Examine the contents of hank
+hank # uma lista
+
+# Convert the hank list into a data frame
+as_data_frame(hank) %>% 
+  # Extract songs where peak equals 1
+  filter(peak == 1)
+
+# Examine the contents of michael
+michael
+
+# Replace the first line so each album has its own rows
+bind_rows(michael, .id = "album") %>% 
+  group_by(album) %>% 
+  mutate(rank = min_rank(peak)) %>% 
+  filter(rank == 1) %>% 
+  select(-rank, -peak)
